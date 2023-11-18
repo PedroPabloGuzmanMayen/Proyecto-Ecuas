6b66c180a942b3fbdc49082b5cbdfa422af6c9c2
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2023-11-17 21:42:11 -0600

    Libraries

--- ProyectoFInal.py
+++ ProyectoFInal.py
@@ -1,3 +1,16 @@
-#Gustavo Adolfo Cruz
-#Joaquín Campos
-#Pedro Pablo Guzmán
+#Gustavo Adolfo Cruz 22779
+#Joaquín Campos 22155
+#Pedro Pablo Guzmán 22111
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sympy as smp
+import scipy as sp
+from scipy.integrate import quad
+import tkinter as tk
+from tkinter import messagebox
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from mpl_interactions import ioff, panhandler, zoom_factory
+
+
